26c0a69b31f0cb4611d83e5113f6e2737ed34c65
mineduck1608 <ducdm160804@gmail.com>
2025-11-01 11:42:17 +0700

    init

---
+++ src/custom.d.ts
@@ -0,0 +1,7 @@
+// declare common asset modules so TypeScript won't complain about imports like `logo.svg`
+declare module '*.svg';
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.css';
